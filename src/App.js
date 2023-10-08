bdeafd871a8b6ffbfd62cefd5a60afddc9ae95f1
janani <jananishakthi2017@gmail.com>
2023-10-08 18:28:12 +0530

    new file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import './Styles.css';
-import Navbar from './components/Navbar';
 import Home from './routes/Home';
 import Contact from './routes/Contact';
 import Service from './routes/Service';
@@ -9,13 +9,16 @@ import Sign from './routes/Sign';
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path='/' element={<Home />}/>
-        <Route path='/about' element={<About />}/>
-        <Route path='/service' element={<Service />}/>
-        <Route path='/contact' element={<Contact />}/>
-        <Route path='/sign' element={<Sign />}/>
-      </Routes>
+      <BrowserRouter basename='/React-Travel'>
+          <Routes>
+            <Route exact path='/React-Travel' element={<Home />}/>
+            <Route path='/about' element={<About />}/>
+            <Route path='/service' element={<Service />}/>
+            <Route path='/contact' element={<Contact />}/>
+            <Route path='/sign' element={<Sign />}/>
+          </Routes>
+      </BrowserRouter>
+      
       
       
     </div>
